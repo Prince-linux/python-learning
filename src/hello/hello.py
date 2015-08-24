f6f18696d5740305a7e60871bb4f71fd5b7222e1
Prince Oppong Boamah <regioths@gmail.com>
2015-08-24 20:16:05 +0000

    Created the script hello.py . Closes #1

---
+++ src/hello/hello.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+
+# hello.py -- This script prints Hello World! to the screen.
+# Author -- Prince Oppong Boamah<regioths@gmail.com>
+# Date 24th August 2015.
+
+print ("Hello World!")
+
