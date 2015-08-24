735bad7e62fc6e13f97b7ad76b163341bbd77928
Prince Oppong Boamah <regioths@gmail.com>
2015-08-24 22:50:26 +0000

    Created the script add.py and wiki. Closes #4

---
+++ src/add/add.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+# add.py -- This script asks for two inputs from a user, adds them and prints them to the scree.
+# Author -- Prince Oppong Boamah<regioths@gmail.com>
+# Date -- 24th August 2015
+
+num1 = input("Please enter your first number: ")
+num2 = input("Please enter your second number: ")
+
+print(num1+num2)
+
