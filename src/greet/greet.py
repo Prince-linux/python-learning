4da240060bcdec2fc46c3f05ee4f82d35f5e1a5a
Prince Oppong Boamah <regioths@gmail.com>
2015-08-24 20:28:02 +0000

    Created the script greet.py . Closes #2

---
+++ src/greet/greet.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+# greet.py -- This scripts asks for a username and then greets the user afterwards.
+
+# Author -- Prince Oppong Boamah<regioths@gmail.com>
+
+# Date -- 24th August 2015
+
+name = input("What is your name? ")
+print("Hello %s!" % (name))
+
