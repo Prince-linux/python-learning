ac7a376a28a74bb4e590b9da9f1ce3912a511718
Prince Oppong Boamah <regioths@gmail.com>
2015-08-24 23:43:40 +0000

    Created the Scripts days-of-week.py and months-of-the-year.py . Closes #5

---
+++ src/lists/days-of-week.py
@@ -0,0 +1,4 @@
+week = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+for day in week:
+    print(" This is a  day of the week called %s." % (day))
+
---
+++ src/lists/months-of-the-year.py
@@ -0,0 +1,4 @@
+year = ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+for month in year:
+    print(" This is a  month of the year called %s." % (month))
+
