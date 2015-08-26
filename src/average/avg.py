6b5731608e68e861c59c1ad91fd878ef0ccd473d
Prince Oppong Boamah <regioths@gmail.com>
2015-08-26 16:02:57 +0000

    Created the script avg.py .

---
+++ src/average/avg.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python3
+
+
+# avg.py -- This script asks the user to enter ten numbers, then prints their average.
+
+# Author -- Prince Oppong Boamah<regioths@gmail.com>
+# Date -- 26th August, 2015
+
+a = input("Please enter any ten numbers of your choice: ")
+x = int(a)
+print(x)
+b = input("Next: ")
+x = int(b)
+print(x)
+c = input("Next: ")
+x = int(c)
+print(x)
+d = input("Next: ")
+x = int(d)
+print(x)
+e = input("Next: ")
+x = int(e)
+print(x)
+f = input("Next: ")
+x = int(f)
+print(x)
+g = input("Next: ")
+x= int(g)
+print(x)
+h = input("Next: ")
+x = int(h)
+print(x)
+i = input("Next: ")
+x = int(i)
+print(x)
+j = input("Next: ")
+x = int(j)
+print(x)
+
+
+
+
+
+
