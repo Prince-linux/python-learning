0533fb75577a99bb03ac335e9aec0bf25cadc673
Prince Oppong Boamah <regioths@gmail.com>
2015-08-26 17:47:59 +0000

    Modified the script add.py . Closes #7

--- src/add/add.py
+++ src/add/add.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python3
+import sys
 
 # add.py -- This script asks for two inputs from a user, adds them and prints them to the scree.
 # Author -- Prince Oppong Boamah<regioths@gmail.com>
@@ -6,6 +7,11 @@
 
 num1 = input("Please enter your first number: ")
 num2 = input("Please enter your second number: ")
+try:
+    a = int(num1)
+    b = int(num2)
+    print(a+b)
+except ValueError:
+    print("That is not a valid integer")
 
-print(num1+num2)
 
