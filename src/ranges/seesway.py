76f66ae7d6cfc4d2548eb6e95e1cfe65c1b168a2
Prince Oppong Boamah <regioths@gmail.com>
2015-08-27 23:48:05 +0000

    Modified the script seesway.py . Closes #3

--- src/ranges/seesway.py
+++ src/ranges/seesway.py
@@ -1,4 +1,19 @@
 #! /usr/bin/python3
+
+# seesway.py -- This script counts from -10 to 10 and then back from 10 to -10
+
+# Author -- Prince Oppong Boamah<regioths@gmail.com>
+
+# Date -- 27th August 2015
+
 for i in range(-10, 11): print(i)
-for i in range(10, -11): print(i)
+for i in range(9, -1, -1): print(i)
+for i in range(-10, 0): print(i)
+
+
+
+        
+        
+
+
 
