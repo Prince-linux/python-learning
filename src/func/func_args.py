aa809ed725f7b947af0ff03a476ec69045a7f073
Prince Oppong Boamah <regioths@gmail.com>
2015-08-28 02:35:11 +0000

    Created the script func_args.py . Closes #9

---
+++ src/func/func_args.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3
+
+# func_args.py -- This script takes two numbers and prints whether they are equal, or one is greather or less than the other.
+
+# Author -- Prince Oppong Boamah<regioths@gmail.com>
+
+# Date -- 27th August, 2015
+def compare_nums():
+    print()
+
+a = 43
+b = 53
+
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a and b are equal")
+
+
+    
+
+
+    
+
+
+
