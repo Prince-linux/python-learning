76ccc827270cab1b3c4dffe1a22d965b9a75f98b
Prince Oppong Boamah <regioths@gmail.com>
2015-08-28 01:14:51 +0000

    Created the script func.py . Closes #8

---
+++ src/func/func.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+# func.py -- This script calls the hello function 10 times.
+
+# Author -- Prince Oppong Boamah<regioths@gmail.com>
+
+# Date -- 27th August, 2015
+
+def hello():
+    print("Hello world!")
+
+print("I am going to call the hello function 10 times")
+for i in range(10):
+    print("hello")
+
