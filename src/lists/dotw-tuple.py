031216301986aaa4ebb0e26ee51d3bd3f468de9b
Prince Oppong Boamah <regioths@gmail.com>
2015-09-09 02:31:15 +0000

    Created the scripts dotw-tuple.py and moty-tuple.py . Closes #13

---
+++ src/lists/dotw-tuple.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+# dotw-tuple.py -- This script prints out the days of the week.
+
+# Author -- prince Oppong Boamah<regioths@gmail.com>
+
+# Date -- 8th September, 2015
+
+week = ('Sun','Mon', 'Tue', 'Wednes', 'Thur', 'Fri', 'Satur')
+for day in week:
+    print("This is a day of the week called %sday." % (day))
+
---
+++ src/lists/moty-tuple.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+# moty-tuple.py -- This prints months of the year to the screen.
+
+# Author -- prince Oppong Boamah<regioths@gmail.com>
+
+# Date -- 8th September, 2015
+
+year = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+for month in year:
+    print(" This is a month of the year called %s." % (month))
+
