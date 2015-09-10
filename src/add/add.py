215a4d3feb47a91e2c7f8ab90c05cb341bf5445d
Prince Oppong Boamah <regioths@gmail.com>
2015-09-10 16:30:49 +0000

    Modified the script add.py . Closes #21

--- src/add/add.py
+++ src/add/add.py
@@ -1,18 +1,27 @@
 #! /usr/bin/python3
 
+# add.py -- This script asks for two inputs from a user, adds them and prints them to the screen.
 
-# add.py -- This script asks for two inputs from a user, adds them and prints them to the scree.
 # Author -- Prince Oppong Boamah<regioths@gmail.com>
-# Date -- 24th August 2015
+
+# Date -- 10th september, 2015
 
 while True:
     a = input("Please enter any integer: ")
-    b = input("Please enter another integer: ")
     try:
         n1 = int(a)
-        n2 = int(b)
-        print(n1+n2)
+        print(n1)
         break
     except ValueError:
-        print("Not a valid integer:  Try again." )
+        print("Not a valid integer:  Try again.")
 
+             
+while True:
+    b = input("Please enter another integer: ")
+    try:
+        n2 = int(b)
+        print(n1 + n2)
+        break
+    except ValueError:
+        print("Not a valid integer: Try again.")
+    
