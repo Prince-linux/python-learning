97207886ed497b1b68d7bd6aefffd96526953b16
Prince Boamah <princeboamah@Princes-MacBook-Air.local>
2020-03-05 16:01:55 +0000

    Prince: A function to greet user

---
+++ src/polls/greet.py
@@ -0,0 +1,12 @@
+# A simple use of user defined functions in python
+
+
+def greet_user(name):
+    print(f"Hi {name}!")
+    print("Welcome Aboard!")
+
+
+print("Start")
+greet_user("Kwadwo")
+greet_user("Sammy")
+print("finish")
